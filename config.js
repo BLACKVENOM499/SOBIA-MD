2e269ae56e358c2bd1a9ddeef6ab0aa2cbf281f1
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-05-21 20:31:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "yI0yjLKR#y3CtvtSwTilogZLc6cOALWRjqeStFLF1melzIz8_8KA",
     CAPTION: process.env.CAPTION || "POWERED BY SOBIA BUTT",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
